bf1bfdae0e2fe63deeb486317b8b23059fb0a84d
Oleksandra Falieieva <alexandravro@gmail.com>
2026-09-06 12:04:54 +0300

    homework-13: implement Silver and Gold dbt models for medallion pipeline

--- lesson-13-spark-structured-data-2/homework/dbt_lakehouse/models/gold/fact_repo_activity_daily.sql
+++ lesson-13-spark-structured-data-2/homework/dbt_lakehouse/models/gold/fact_repo_activity_daily.sql
@@ -7,16 +7,118 @@
 --          distinct_committers, prs_opened, prs_merged, issues_opened, issues_closed, stars, forks.
 
 -- TODO: замініть заглушку на запит згідно зі SPEC.md
+with commit_daily as (
+    select
+        md5(repo_name)                                      as repo_id,
+        cast(date_format(pushed_at, 'yyyyMMdd') as int)     as date_id,
+        count(*)                                            as commits,
+        count(distinct author_email)                        as distinct_committers,
+        0                                                   as prs_opened,
+        0                                                   as prs_merged,
+        0                                                   as issues_opened,
+        0                                                   as issues_closed,
+        0                                                   as stars,
+        0                                                   as forks
+    from {{ ref('commits') }}
+    group by repo_id, date_id
+),
+
+pr_daily as (
+    select
+        md5(repo_name)                                      as repo_id,
+        cast(date_format(opened_at, 'yyyyMMdd') as int)     as date_id,
+        0                                                   as commits,
+        0                                                   as distinct_committers,
+        count(*)                                            as prs_opened,
+        0                                                   as prs_merged,
+        0                                                   as issues_opened,
+        0                                                   as issues_closed,
+        0                                                   as stars,
+        0                                                   as forks
+    from {{ ref('pull_requests') }}
+    group by repo_id, date_id
+    union all
+    select
+        md5(repo_name)                                      as repo_id,
+        cast(date_format(merged_at, 'yyyyMMdd') as int)     as date_id,
+        0, 0, 0,
+        count(*)                                            as prs_merged,
+        0, 0, 0, 0
+    from {{ ref('pull_requests') }}
+    where merged_at is not null
+    group by repo_id, date_id
+),
+
+issue_daily as (
+    select
+        md5(repo_name)                                      as repo_id,
+        cast(date_format(opened_at, 'yyyyMMdd') as int)     as date_id,
+        0, 0, 0, 0,
+        count(*)                                            as issues_opened,
+        0, 0, 0
+    from {{ ref('issues') }}
+    group by repo_id, date_id
+    union all
+    select
+        md5(repo_name)                                      as repo_id,
+        cast(date_format(closed_at, 'yyyyMMdd') as int)     as date_id,
+        0, 0, 0, 0, 0,
+        count(*)                                            as issues_closed,
+        0, 0
+    from {{ ref('issues') }}
+    where closed_at is not null
+    group by repo_id, date_id
+),
+
+watch_fork_daily as (
+    select
+        md5(repo_name)                                      as repo_id,
+        cast(date_format(created_at, 'yyyyMMdd') as int)    as date_id,
+        0, 0, 0, 0, 0, 0,
+        sum(case when event_type = 'WatchEvent' then 1 else 0 end) as stars,
+        sum(case when event_type = 'ForkEvent' then 1 else 0 end)  as forks
+    from {{ ref('events') }}
+    where event_type in ('WatchEvent', 'ForkEvent')
+    group by repo_id, date_id
+),
+
+combined as (
+    select * from commit_daily
+    union all
+    select * from pr_daily
+    union all
+    select * from issue_daily
+    union all
+    select * from watch_fork_daily
+),
+
+aggregated as (
+    select
+        repo_id,
+        date_id,
+        sum(commits)                                        as commits,
+        sum(distinct_committers)                            as distinct_committers,
+        sum(prs_opened)                                     as prs_opened,
+        sum(prs_merged)                                     as prs_merged,
+        sum(issues_opened)                                  as issues_opened,
+        sum(issues_closed)                                  as issues_closed,
+        sum(stars)                                          as stars,
+        sum(forks)                                          as forks
+    from combined
+    group by repo_id, date_id
+)
+
 select
-    cast(null as string) as activity_id,
-    cast(null as string) as repo_id,
-    cast(null as int)    as date_id,
-    cast(null as bigint) as commits,
-    cast(null as bigint) as distinct_committers,
-    cast(null as bigint) as prs_opened,
-    cast(null as bigint) as prs_merged,
-    cast(null as bigint) as issues_opened,
-    cast(null as bigint) as issues_closed,
-    cast(null as bigint) as stars,
-    cast(null as bigint) as forks
-where false
+    md5(concat_ws('|', repo_id, cast(date_id as string)))  as activity_id,
+    repo_id,
+    date_id,
+    commits,
+    distinct_committers,
+    prs_opened,
+    prs_merged,
+    issues_opened,
+    issues_closed,
+    stars,
+    forks
+from aggregated
+
--- lesson-13-spark-structured-data-2/homework/dbt_lakehouse/tests/assert_daily_rollup_matches_facts.sql
+++ lesson-13-spark-structured-data-2/homework/dbt_lakehouse/tests/assert_daily_rollup_matches_facts.sql
@@ -1,5 +1,9 @@
 -- Тест: sum(fact_repo_activity_daily.commits) = count(*) з fact_commit.
 -- Специфікація: ../../SPEC.md → «Тести». Тест падає, якщо запит поверне рядки.
 -- TODO: замініть заглушку (зараз тест проходить вхолосту).
-select 1 as _placeholder
-where false
+select 1
+where (
+    select sum(commits) from {{ ref('fact_repo_activity_daily') }}
+) != (
+    select count(*) from {{ ref('fact_commit') }}
+)
